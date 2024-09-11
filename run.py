aa9bb20ad06b968495b26465a1d7fd704975f01c
ccheemeng <e0538150@u.nus.edu>
2024-09-11 23:45:41 +0800

    Fix #6 for real this time, update .gitignore and environment.yml

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import aiocsv
+import aiofiles
 from aiohttp import ClientSession, ClientTimeout
 import argparse
 import asyncio
@@ -8,39 +10,39 @@ import streetview
 import csv
 from pathlib import Path
 
-def main(p1, p2, resolution, target_crs, filepath):
-    p1 = tuple(p1)
-    p2 = tuple(p2)
+async def main(p1, p2, resolution, target_crs, filepath):
     Path(filepath).parent.mkdir(parents=True, exist_ok=True)
     with open(filepath, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['x', 'y', 'z'])    
-    panos = asyncio.run(write_panos(p1, p2, resolution, target_crs, filepath))
-
-async def write_panos(p1, p2, resolution, target_crs, filepath):
-    sample = geo.Sample(p1, p2, resolution)
-    radius = sample.search_radius()
-    async with ClientSession(timeout=ClientTimeout(total=None), raise_for_status=True) as session:
-        tasks = []
-        ids = set()
-        for lat, lon in sample.generate_latlon_samples():
-            tasks.append(write_pano_xyz(ids, await streetview.StreetViewAPI.find_pano_full(session, lat, lon, radius), target_crs, filepath))
-        await asyncio.gather(*tasks)
+        csv_writer.writerow(['x', 'y', 'z'])
     
-async def write_pano_xyz(ids, pano, target_crs, filepath):
-    if pano and pano.elevation and pano.depth and pano.id not in ids:
+    p1 = tuple(p1)
+    p2 = tuple(p2)
+    sample = geo.Sample(p1, p2, resolution)
+    latlon = list(sample.generate_latlon_samples())
+    lat = [x[0] for x in latlon]
+    lon = [x[1] for x in latlon]
+    radius = sample.search_radius()
+    await write_panos(lat, lon, radius, target_crs, filepath)
+
+async def write_panos(lat, lon, radius, target_crs, filepath):
+    ids = set()
+    async with ClientSession(timeout=ClientTimeout(total=None), raise_for_status=True) as session:
+        async with aiofiles.open(filepath, 'w+') as afp:
+            for i in range(len(lat)):
+                async for row in get_xyz(session, ids, lat[i], lon[i], radius, target_crs):
+                    writer = aiocsv.AsyncWriter(afp)
+                    await writer.writerow(row)
+    
+async def get_xyz(session, ids, lat, lon, radius, target_crs):
+    pano = await streetview.StreetViewAPI.find_pano_full(session, lat, lon, radius)
+    if pano and pano.id not in ids and pano.elevation and pano.depth:
         ids.add(pano.id)
         xyz_4326 = (pano.lon, pano.lat, pano.elevation - pano.depth.data[-1][0])
         transformer = geo.Transform(4326, target_crs)
         xyz_target = transformer.transform3d(xyz_4326)
         print(xyz_target)
-        loop = asyncio.get_running_loop()
-        await loop.run_in_executor(None, writerow, xyz_target, filepath)
-
-def writerow(row, filepath):
-    with open(filepath, 'a+', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(row)
+        yield xyz_target
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -64,4 +66,4 @@ if __name__ == "__main__":
                         dest="filepath")
     args = parser.parse_args()
 
-    main(args.p1, args.p2, args.resolution, args.target_crs, args.filepath)
+    asyncio.run(main(args.p1, args.p2, args.resolution, args.target_crs, args.filepath))
